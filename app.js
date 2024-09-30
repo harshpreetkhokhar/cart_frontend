a58cf122b9a0e431ac04829433ff3371141ad81c
Harsh Preet Kaur <harsh.kaur@cloudeq.com>
2024-10-01 02:01:13 +0530

    1

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ import express from 'express';
 const app = express();
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const buildPath = path.join(__dirname, "/dist");
+const buildPath = path.join(__dirname,"/");
 
 app.use(express.static(buildPath));
 
 app.get("/*", function (req, res) {
     res.sendFile(
-        path.join(__dirname, "/dist/index.html"),
+        path.join(__dirname, "/index.html"),
         function (err) {
             if (err) {
                 res.status(500).send(err);
