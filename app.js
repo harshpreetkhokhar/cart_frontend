55d1a7e31f2613cfd5a550ff7c194646c5b6eb36
Harsh Preet Kaur <harsh.kaur@cloudeq.com>
2024-10-01 01:15:37 +0530

    !!

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+import path from 'path';
+import express from 'express';
 
-const path = require('path')
+const app = express();
 
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "/dist");
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const buildPath = path.join(__dirname, "/dist");
 
-app.use(express.static(buildPath))
+app.use(express.static(buildPath));
 
-app.get("/*", function(req, res){
-
+app.get("/*", function (req, res) {
     res.sendFile(
         path.join(__dirname, "/dist/index.html"),
         function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
+            if (err) {
+                res.status(500).send(err);
+            }
         }
-      );
+    );
+});
 
-})
+// Start the server (make sure to specify your port)
+const PORT = process.env.PORT || 5173;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
